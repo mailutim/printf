76f5fd8bfa4d5aa4c789d0e624a9feccc404cc79
Mozzie <joshuamutiso455@gmail.com>
2022-04-18 11:15:24 +0300

    task0

---
+++ _putchar.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+
+/**
+* _putchar - write a character
+* @c: the character to print to STDOUT
+* Return: int
+*/
+int _putchar(char c)
+{
+    return(write( 1, &c, 1)) ;
+
+}
+
